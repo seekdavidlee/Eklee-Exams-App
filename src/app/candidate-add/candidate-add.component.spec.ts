c215320064264b76b6207f3a4fda9b5c011b2228
David Lee <seekdavidlee@gmail.com>
2019-01-27 20:17:56 -0600

    Fix broken tests

--- src/app/candidate-add/candidate-add.component.spec.ts
+++ src/app/candidate-add/candidate-add.component.spec.ts
@@ -1,16 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CandidateAddComponent } from './candidate-add.component';
+import { FormsModule } from '@angular/forms';
+import { GraphqlConfig, GraphqlService } from '../graphql.service';
 
 describe('CandidateAddComponent', () => {
   let component: CandidateAddComponent;
   let fixture: ComponentFixture<CandidateAddComponent>;
+  let graphqlService = {
 
+    getConfig: (): GraphqlConfig => {
+      let config = new GraphqlConfig();
+      config.url = "http://fo";
+      return config;
+    },
+
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CandidateAddComponent ]
+      imports: [FormsModule],
+      declarations: [CandidateAddComponent],
+      providers: [
+        { provide: GraphqlService, useValue: graphqlService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/graphql.service.spec.ts
+++ src/app/graphql.service.spec.ts
@@ -1,15 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { GraphqlService } from './graphql.service';
+import { GraphqlService, GraphqlConfig } from './graphql.service';
+import { of } from 'rxjs';
+import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
 
 describe('GraphqlService', () => {
   let httpMock: HttpTestingController;
   let service: GraphqlService;
 
+  let graphqlService = {
+
+    getConfig: (): GraphqlConfig => {
+      let config = new GraphqlConfig();
+      config.url = "http://fo";
+      return config;
+    },
+
+  };
+  
+  let adalSvc = {
+    username: "f",
+    profile: {
+      tid: '12345'
+    },
+    acquireToken: (url: string) => {
+      return of();
+    }
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [GraphqlService]
+      providers: [
+        { provide: GraphqlService, useValue: graphqlService },
+        { provide: MsAdalAngular6Service, useValue: adalSvc }
+      ]
     });
 
     service = TestBed.get(GraphqlService);
